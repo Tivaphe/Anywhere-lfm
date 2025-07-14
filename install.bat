f4186522bb735be0d595c87a23cc382b1173a29d
Tivaphraen <varainetiphen@gmail.com>
2025-07-15 00:49:48 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,8 +6,8 @@ echo #                                                               #
 echo #################################################################
 echo.
 
-REM Vérifier les prérequis importants
-echo Vérification des prérequis...
+REM Verifier les prerequis importants
+echo Verification des prerequis...
 python --version >nul 2>nul
 if %errorlevel% neq 0 (
     echo Python n'est pas installé ou n'est pas dans le PATH.
