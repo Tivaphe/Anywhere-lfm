a834956f1aae25ea1b2886902e439f891eb41f79
Tivaphraen <varainetiphen@gmail.com>
2025-07-19 19:17:49 +0200

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Lancement de l'interface graphique LiquidAI...
-
-REM Activer l'environnement virtuel
-call venv\\Scripts\\activate
-
-REM Lancer l'application principale
-python main.py
